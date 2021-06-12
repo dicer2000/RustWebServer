a2c8180b21ec5fb5b60343105e4965a1024c6d16
Brett Huffman <dicer2000@yahoo.com>
2021-06-11 22:15:12 -0500

    RustWS

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // Step 1 : Show how connections get established
+        println!("Connection established!");
+        // Step 2: Handle the connection
+        handle_connection(stream);
+
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    }
+}
